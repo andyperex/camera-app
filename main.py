42832159ef81162f4b9f937611855b76d80e3e5a
Andres perez <andres.perezmail@yahoo.com>
2024-12-01 19:14:01 -0300

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+from PIL import Image
+
+st.title("Do you want to look like a model?")
+
+with st.expander("Start Camera"):
+    camera_image = st.camera_input("Camera")
+
+if camera_image:
+
+    img = Image.open(camera_image)
+    gray_img = img.convert("L")
+
+    st.image(gray_img)
